980a92d11f8a7d828dbdee9567d66242603b319c
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-18 13:15:44 +0000

    minimal microui non-rendering example

--- poc-microui.cpp
+++ poc-microui.cpp
@@ -8,15 +8,71 @@ extern "C" {
 #include "microui/src/microui.h"
 }
 
-static auto init_mu_ctx() {
+static auto init() {
   auto ctx = new mu_Context {};
   mu_init(ctx);
   return ctx;
 }
-static auto deinit_mu_ctx(mu_Context * ptr) {
+static auto deinit(mu_Context * ptr) {
   delete ptr;
 }
 
+static int text_width(mu_Font, const char *, int len) {
+  return 5 * len;
+}
+static int text_height(mu_Font) {
+  return 8;
+}
+
 int main() {
-  hay<mu_Context *, init_mu_ctx, deinit_mu_ctx> ctx {}; 
+  hay<mu_Context *, init, deinit> ctx {}; 
+  ctx->text_width = text_width;
+  ctx->text_height = text_height;
+
+  mu_begin(ctx);
+
+  auto wnd_rect = mu_rect(10, 10, 300, 400);
+  //auto wnd_opts = MU_OPT_NOCLOSE | MU_OPT_NOTITLE;
+  auto wnd_opts = 0;
+  if (mu_begin_window_ex(ctx, "Window", wnd_rect, wnd_opts)) {
+    int cols[] { 100, 100 };
+    mu_layout_row(ctx, 2, cols, 0);
+
+    mu_text(ctx, "One");
+    if (mu_button(ctx, "Hello")) putln("hello");
+
+    mu_text(ctx, "Two");
+    if (mu_button(ctx, "World")) putln("world");
+
+    mu_end_window(ctx);
+  }
+
+  mu_end(ctx);
+
+  mu_Command * cmd {};
+  while (mu_next_command(ctx, &cmd)) {
+    switch (cmd->type) {
+      case MU_COMMAND_CLIP: {
+        auto [x, y, w, h] = cmd->clip.rect;
+        putan("clip", x, y, w, h);
+        break;
+      }
+      case MU_COMMAND_ICON: {
+        auto [x, y, w, h] = cmd->icon.rect;
+        putan("icon", cmd->icon.id, x, y, w, h);
+        break;
+      }
+      case MU_COMMAND_RECT: {
+        auto [x, y, w, h] = cmd->rect.rect;
+        putan("rect", x, y, w, h);
+        break;
+      }
+      case MU_COMMAND_TEXT:
+        putan("text", cmd->text.pos.x, cmd->text.pos.y, cmd->text.str);
+        break;
+      default:
+        putan("cmd", cmd->type);
+        break;
+    }
+  }
 }
