da91587047989458394c25283202b18ea1135f89
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-20 15:07:08 +0000

    experimenting with voo renderers

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,7 @@
 #pragma leco app
 
 import casein;
+import hai;
 import vinyl;
 import voo;
 
@@ -10,20 +11,29 @@ struct as {
   voo::device_and_queue dq { "poc", casein::native_ptr };
 };
 struct ss {
-  //vee::render_pass rp = voo::single_att_render_pass(g.as()->dq);
+  vee::render_pass rp = voo::single_att_render_pass(g.as()->dq);
   voo::swapchain sw { g.as()->dq };
   voo::single_cb cb {};
+  hai::array<vee::framebuffer> fbs = sw.create_framebuffers(*rp);
+  hai::array<vee::render_pass_begin> rpbs = [this] {
+    hai::array<vee::render_pass_begin> res { fbs.size() };
+    for (auto i = 0; i < res.size(); i++) 
+      res[i] = vee::render_pass_begin {
+        .command_buffer = cb.cb(),
+        .render_pass = *rp,
+        .framebuffer = *fbs[i],
+        .extent = sw.extent(),
+        .clear_colours { vee::clear_colour(0.01, 0.02, 0.03, 1.0) },
+      };
+    return res;
+  }();
 };
 
 static void on_frame() {
   auto cb = g.ss()->cb.cb();
 
-  g.ss()->sw.acquire_next_image();
-  {
-    voo::cmd_buf_one_time_submit ots { cb };
-  }
-  g.ss()->sw.queue_submit(cb);
-  g.ss()->sw.queue_present();
+  voo::ots_present_guard pg { &g.ss()->sw, cb };
+  voo::cmd_render_pass rp { g.ss()->rpbs[g.ss()->sw.index()] };
 }
 
 const int i = [] {
