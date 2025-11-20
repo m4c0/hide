aa8fdfebfaecf333b44d76e0c0421bb3bd65a81d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-20 20:09:13 +0000

    drawing boxes again

--- poc.cpp
+++ poc.cpp
@@ -73,8 +73,10 @@ static void on_frame() {
   auto cb = g.ss()->cb.cb();
   auto aspect = g.ss()->sw.aspect();
 
+  auto h = 10.f;
+  auto w = aspect * h;
   upc pc {
-    .extent { aspect * 10.f, 10.f },
+    .extent { w, h },
   };
 
   unsigned count = 0;
@@ -83,12 +85,47 @@ static void on_frame() {
     m += {
       .pos { 0, 0 },
       .size { 1 },
-      .colour { 1 },
+      .colour { 1, 0, 0, 1 },
     };
     m += {
-      .pos { 9, 9 },
+      .pos { 1, 0 },
+      .size { w - 2.f, 1.f },
+      .colour { 0.5f },
+    };
+    m += {
+      .pos { w - 1.f, 0.f },
       .size { 1 },
-      .colour { 1, 0, 0, 1 },
+      .colour { 0, 1, 0, 1 },
+    };
+    m += {
+      .pos { 0, 1 },
+      .size { w, h - 2.f },
+      .colour { 0.2f },
+    };
+    m += {
+      .pos { 0.f, h - 1.f },
+      .size { w / 5.0f, 1.f },
+      .colour { 0.5f, 0.f, 0.f, 1.f },
+    };
+    m += {
+      .pos { w / 5.0f, h - 1.f },
+      .size { w / 5.0f, 1.f },
+      .colour { 0.f, 0.5f, 0.f, 1.f },
+    };
+    m += {
+      .pos { 2.0f * w / 5.0f, h - 1.f },
+      .size { w / 5.0f, 1.f },
+      .colour { 0.f, 0.f, 0.5f, 1.f },
+    };
+    m += {
+      .pos { 3.0f * w / 5.0f, h - 1.f },
+      .size { w / 5.0f, 1.f },
+      .colour { 0.5f, 0.5f, 0.f, 1.f },
+    };
+    m += {
+      .pos { 4.0f * w / 5.0f, h - 1.f },
+      .size { w / 5.0f, 1.f },
+      .colour { 0.5f, 0.f, 0.5f, 1.f },
     };
   }
 
